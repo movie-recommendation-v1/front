6803219d9987bc4487262b99700ce45df3803b11
MuhammadAminShomurodov <kriminaltj@gmail.com>
2024-10-16 19:51:32 +0500

    new

--- src/layout/footer/Footer.jsx
+++ src/layout/footer/Footer.jsx
@@ -1,123 +1,64 @@
-import "./Footer.scss";
-import { NavLink } from "react-router-dom";
 import { FaFacebook, FaInstagram, FaTiktok } from "react-icons/fa";
+import "./Footer.scss";
 
 const Footer = () => {
   return (
     <div className="Footer">
       <div className="container">
-        <div className="footer-section">
-          <h2>MOSTREAM</h2>
+        <div className="footer1">
+          <h2>Betayincha Logo</h2>
           <p>
-            Where every frame tells a story, and every click opens the door to a
+            where every frame tells a story, and every click opens the door to a
             world of limitless entertainment. Immerse yourself in a universe of
-            unparalleled movies.
+            unparalleled movie.
           </p>
         </div>
+
         <div className="footer-links">
           <ul>
             <li>
-              <NavLink
-                to="/"
-                className={({ isActive }) => (isActive ? "active" : "")}
-              >
-                <strong>Home</strong>
-              </NavLink>
+              <a href="#">Home</a>
             </li>
             <li>
-              <NavLink
-                to="/movies"
-                className={({ isActive }) => (isActive ? "active" : "")}
-              >
-                Movies
-              </NavLink>
+              <a href="#">Movies</a>
             </li>
             <li>
-              <NavLink
-                to="/tv-shows"
-                className={({ isActive }) => (isActive ? "active" : "")}
-              >
-                TV Shows
-              </NavLink>
+              <a href="#">TV Shows</a>
             </li>
             <li>
-              <NavLink
-                to="/redeem"
-                className={({ isActive }) => (isActive ? "active" : "")}
-              >
-                Redeem
-              </NavLink>
+              <a href="#">Redeem</a>
             </li>
           </ul>
           <ul>
             <li>
-              <NavLink
-                to="/about"
-                className={({ isActive }) => (isActive ? "active" : "")}
-              >
-                <strong>About</strong>
-              </NavLink>
+              <a href="#">About</a>
             </li>
             <li>
-              <NavLink
-                to="/career"
-                className={({ isActive }) => (isActive ? "active" : "")}
-              >
-                Career
-              </NavLink>
+              <a href="#">Career</a>
             </li>
             <li>
-              <NavLink
-                to="/investors"
-                className={({ isActive }) => (isActive ? "active" : "")}
-              >
-                Investors
-              </NavLink>
+              <a href="#">Investors</a>
             </li>
             <li>
-              <NavLink
-                to="/contact-us"
-                className={({ isActive }) => (isActive ? "active" : "")}
-              >
-                Contact Us
-              </NavLink>
+              <a href="#">Contact Us</a>
             </li>
           </ul>
           <ul>
             <li>
-              <NavLink
-                to="/faq"
-                className={({ isActive }) => (isActive ? "active" : "")}
-              >
-                <strong>FAQ</strong>
-              </NavLink>
+              <a href="#">FAQ</a>
             </li>
             <li>
-              <NavLink
-                to="/help-center"
-                className={({ isActive }) => (isActive ? "active" : "")}
-              >
-                Help Center
-              </NavLink>
+              <a href="#">Help Center</a>
             </li>
             <li>
-              <NavLink
-                to="/media-center"
-                className={({ isActive }) => (isActive ? "active" : "")}
-              >
-                Media Center
-              </NavLink>
+              <a href="#">Media Center</a>
             </li>
             <li>
-              <NavLink
-                to="/cookie-preferences"
-                className={({ isActive }) => (isActive ? "active" : "")}
-              >
-                Cookie Preferences
-              </NavLink>
+              <a href="#">Cookie Preferences</a>
             </li>
           </ul>
         </div>
+
         <div className="social-media">
           <h4>Stay up to date</h4>
           <div className="social-icons">
